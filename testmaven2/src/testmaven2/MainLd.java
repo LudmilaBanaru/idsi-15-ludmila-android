55e20f6c526fac7787006e7c7b8fe674b489f3eb
LudmilaBanaru <ludmilabanaru@gmail.com>
2015-02-19 17:01:57 +0100

    First commit of the project

---
+++ testmaven2/src/testmaven2/MainLd.java
@@ -0,0 +1,13 @@
+package testmaven2;
+
+public class MainLd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello");
+	}
+	public MainLd() {
+		MailEvent mailEvent1 = null;
+	}
+
+}
